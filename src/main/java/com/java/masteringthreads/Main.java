ccc7c464e96d96a56d474d5390fb85299423befa
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-12-27 19:52:07 +0530

    added contract for ThreadPool

--- src/main/java/com/java/masteringthreads/Main.java
+++ src/main/java/com/java/masteringthreads/Main.java
@@ -1,4 +1,4 @@
-package com.javaaiexample;
+package com.java.masteringthreads;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
---
+++ src/main/java/com/java/masteringthreads/threadpoolsynchronization/ThreadPool.java
@@ -0,0 +1,23 @@
+package com.java.masteringthreads.threadpoolsynchronization;
+
+/*
+* implementing Threadpool using synchronization method
+* */
+public interface ThreadPool {
+    /*
+    * submit a task to the thread pool where it will be placed in the queue and picked by
+    * available thread.
+    * */
+    void submit(Runnable task);
+
+    /*
+    * {@return unstarted threads}
+    * */
+    int getRunQueueLength();
+
+    /*
+    * shuts down the thread pool allowing the tasks that have been submitted to complete
+    * no furthur task can be submitted. After the task are completed the thread needs to terminate themselves
+    * */
+    void shutdown();
+}
