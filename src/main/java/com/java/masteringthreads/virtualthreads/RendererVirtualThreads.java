ae2f69508c3d7d9930ecf3edd9a9e79a4216dbf8
Akshit Khatri <akshitkhatri.20@gmail.com>
2025-01-01 13:12:31 +0530

    added unstructured concurreny

--- src/main/java/com/java/masteringthreads/virtualthreads/RendererVirtualThreads.java
+++ src/main/java/com/java/masteringthreads/virtualthreads/RendererVirtualThreads.java
@@ -6,9 +6,18 @@ import com.java.masteringthreads.virtualthreads.renderer.SearchTerm;
 import java.net.http.HttpRequest;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.StructuredTaskScope;
 
+// Run the code and observe how long it takes to complete. It should
+//  be approximately 500 ms. Now change the probabilityOfFailure() method to
+//  return 0.3 and run it again. What do we observe?
+// on failure of one task the other task continue to execute and are not shutdown
+// this can lead to wastage of resources
 //  Change to use StructuredTaskScope.ShutdownOnFailure. Nested tasks
 //  are not an issue with structured tasks. Once we have submitted all the
 //  tasks, we need to join and to then throw the exception if any of the
@@ -23,6 +32,28 @@ public class RendererVirtualThreads extends BasicRenderer {
 
     @Override
     public void renderPage(HttpRequest request) throws Exception {
+        Collection<Future<?>> futures = new ConcurrentLinkedQueue<>();
+        try (var service = Executors.
+                newVirtualThreadPerTaskExecutor()) {
+            futures.add(service.submit(() ->
+                    createAdvertisingLinks(request))); // 3
+            futures.add(service.submit(() -> {
+                List<SearchTerm> terms =
+                        scanForSearchTerms(request); // 1
+                try (var subService = Executors.
+                        newVirtualThreadPerTaskExecutor()) {
+                    terms.forEach(term ->
+                            futures.add(subService.submit(() ->
+                                    collateResult(// 2 & 4
+                                            term.searchOnPartnerSite()))));
+                }
+            }));
+        }
+        System.out.println("done");
+        for (Future<?> future : futures) {
+            future.get();
+        }
+
         try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
             scope.fork(() -> createAdvertisingLinks(request));
             scope.fork(() -> {
